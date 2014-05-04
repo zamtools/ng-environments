dfb178ad957435e5def329588c43b4354a7a4f92
Ian Zamojc <ian@thesecretlocation.net>
2014-05-04 04:14:47 -0400

    simplified get

--- dist/ng-environments.js
+++ dist/ng-environments.js
@@ -1,25 +1,23 @@
-/*! ng-environments - v1.0.0 - 2014-02-27
+/*! ng-environments - v1.0.1 - 2014-05-04
 * https://github.com/zamtools/ng-environments
 * Copyright (c) 2014 Zamtools Inc.; Licensed MIT */
 var app = angular.module('ng-environments', []);
 
 app.provider('$environment', function() {
     this.environments = [];
-    return {
-        $get: function() {
-            if (this.environments.length === 0) {
-                throw 'No environments have been configured';
-            }
-
-            for (var i = 0; i < this.environments.length; i++) {
-                var env = this.environments[i];
-                if (location.href.match(env.pattern)) {
-                    return env;
-                }
-            }
-
-            // if no environment matches
-            throw 'URL does not match any environments';
+    this.$get = function() {
+        if (this.environments.length === 0) {
+            throw 'No environments have been configured';
         }
+
+        for (var i = 0; i < this.environments.length; i++) {
+            var env = this.environments[i];
+            if (location.href.match(env.pattern)) {
+                return env;
+            }
+        }
+
+        // if no environment matches
+        throw 'URL does not match any environments';
     };
 });
--- src/ng-environments.js
+++ src/ng-environments.js
@@ -2,21 +2,19 @@ var app = angular.module('ng-environments', []);
 
 app.provider('$environment', function() {
     this.environments = [];
-    return {
-        $get: function() {
-            if (this.environments.length === 0) {
-                throw 'No environments have been configured';
-            }
-
-            for (var i = 0; i < this.environments.length; i++) {
-                var env = this.environments[i];
-                if (location.href.match(env.pattern)) {
-                    return env;
-                }
-            }
-
-            // if no environment matches
-            throw 'URL does not match any environments';
+    this.$get = function() {
+        if (this.environments.length === 0) {
+            throw 'No environments have been configured';
         }
+
+        for (var i = 0; i < this.environments.length; i++) {
+            var env = this.environments[i];
+            if (location.href.match(env.pattern)) {
+                return env;
+            }
+        }
+
+        // if no environment matches
+        throw 'URL does not match any environments';
     };
 });
